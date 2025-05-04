cd5902d2f586deb7092ce219d01590a9606d5ca6
김민재 <redvelvet0625@gmail.com>
2025-05-05 01:01:55 +0900

    setting

---
+++ src/test/java/com/springboot/runaway/RunawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.runaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
